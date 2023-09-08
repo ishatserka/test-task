b8caf679ffd687b939c967722a92572ae5a20579
Ivan Shatserka <shatserka@gmail.com>
2023-09-08 18:21:28 +0300

    Solution

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Game {
 
     private final String words;
@@ -15,6 +18,56 @@ public class Game {
         Response[] response = new Response[LENGTH];
 
         //code here
+
+        // count chars of expected
+        Map<Character, Integer> countCharsOfExpected = new HashMap<>();
+        for(short i = 0; i< LENGTH; i++){
+            Character letter = words.charAt(i);
+            if (countCharsOfExpected.putIfAbsent(letter, 1) != null) {
+                countCharsOfExpected.compute(letter, (k, v) -> v + 1);
+            }
+        }
+
+        // count chars of guess
+        Map<Character, Integer> countCharsOfGuess = new HashMap<>();
+        for(short i = 0; i< LENGTH; i++){
+            Character letter = guess.charAt(i);
+            if (countCharsOfGuess.putIfAbsent(letter, 1) != null) {
+                countCharsOfGuess.compute(letter, (k, v) -> v + 1);
+            }
+        }
+
+        for(short i=0; i<LENGTH; i++){
+            char letter = guess.charAt(i);
+            if (letter == words.charAt(i)) {
+                response[i] = new Response(i, Response.Color.GREEN);
+                //exclude letter from countCharsOfExpected and countCharsOfGuess as they are matched
+                countCharsOfExpected.compute(letter, (k, v) -> v - 1);
+                countCharsOfGuess.compute(letter, (k, v) -> v - 1);
+            } else if(!words.contains(String.valueOf(letter))){
+                response[i] = new Response(i, Response.Color.GRAY);
+            }
+        }
+
+        for(short i=0; i<LENGTH; i++){
+            if (response[i] != null) {
+                continue;
+            }
+            Character letter = Character.valueOf(guess.charAt(i));
+            // if count of letter in guess == count of letter in expected => YELLOW
+            if(countCharsOfGuess.get(letter) == countCharsOfExpected.get(letter)){
+                response[i] = new Response(i, Response.Color.YELLOW);
+            }
+            // if count of letter in guess > count of letter in expected and expected contains enough needed letters => YELLOW and decrease count of letter in expected
+            else if(countCharsOfGuess.get(letter) > countCharsOfExpected.get(letter) && countCharsOfExpected.get(letter) > 0){
+                response[i] = new Response(i, Response.Color.YELLOW);
+                countCharsOfExpected.compute(letter, (k, v) -> v - 1);
+            }
+            // if count of letter in guess < count of letter in expected or expected doesn't contain enough needed letters => GRAY
+            else {
+                response[i] = new Response(i, Response.Color.GRAY);
+            }
+        }
 
         return response;
     }
